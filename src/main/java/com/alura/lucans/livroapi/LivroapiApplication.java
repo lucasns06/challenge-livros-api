5bd41c375c3c25bf65ac02a0116c7023be318813
lucasns06 <lucasnascimento.carreira@gmail.com>
2025-08-04 10:26:36 -0300

    upload

---
+++ src/main/java/com/alura/lucans/livroapi/LivroapiApplication.java
@@ -0,0 +1,13 @@
+package com.alura.lucans.livroapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivroapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivroapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/lucans/livroapi/LivroapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.lucans.livroapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivroapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
